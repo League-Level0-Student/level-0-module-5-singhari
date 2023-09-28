7bfb1651895fe5859ec4ffb9a237f512a976f00b
league <karinabriellen@gmail.com>
2023-09-27 20:02:50 -0700

    whdafihrgvtrleilr

--- src/_03_method_writing/_1_obedient_robot/Wikipedia.java
+++ src/_03_method_writing/_1_obedient_robot/Wikipedia.java
@@ -1,5 +1,50 @@
 package _03_method_writing._1_obedient_robot;
 
-public class Wikipedia {
+import org.jointheleague.graphical.robot.Robot;
 
+public class Wikipedia {
+	public static void main(String[] args) {
+	//drawSquare();
+	//	drawTriangle();
+		drawCircle();
+	}
+	public static void drawSquare() {
+		Robot robt = new Robot();
+		int start = 300;
+		robt.setX(start);
+		robt.setY(start);
+		robt.penDown();
+		robt.setSpeed(5);
+		robt.setPenColor(0,0,0);
+		for(int x = 0; x<4; x++) {
+			robt.move(100);
+			robt.turn(90);
+		}
+	}
+	public static void drawTriangle() {
+		Robot robt = new Robot();
+		int start = 300;
+		robt.setX(start);
+		robt.setY(start);
+		robt.penDown();
+		robt.setSpeed(5);
+		robt.setPenColor(0,0,0);
+		for(int x = 0; x<3; x++) {
+			robt.turn(120);
+			robt.move(100);
+		}
+	}
+	public static void drawCircle() {
+		Robot robt = new Robot();
+		int start = 300;
+		robt.setX(start);
+		robt.setY(start);
+		robt.penDown();
+		robt.setSpeed(5);
+		robt.setPenColor(0,0,0);
+		for(int x = 0; x<12672; x++) {
+			robt.turn(1);
+			robt.move(5);
+		}
+	}
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,7 +5,7 @@ public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
 String pet = JOptionPane.showInputDialog(null, "What kind of pet do you have?");
@@ -15,15 +15,31 @@ String pet = JOptionPane.showInputDialog(null, "What kind of pet do you have?");
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+					new String[] { "Playtime", "Nutertion", "Take a walk" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
-
+if(task == 0) {
+	Playtime();
+}
+else if (task==1) {
+	 Nuteration();
+}
+else {
+	
+}
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
 	}
-
+	public static void Playtime() {
+	System.out.print(12);
+	}
+	public static void Nuteration() {
+		System.out.print(2);
+	}
+	public static void takeawalk() {
+		
+	}
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
