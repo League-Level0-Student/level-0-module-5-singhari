e65563f02f9acb282a99b099b55a56aef5d30f64
league <karinabriellen@gmail.com>
2023-10-11 20:02:28 -0700

    asdfhk

---
+++ src/_05_self_tests/_6_level0_summary/Cat.java
@@ -0,0 +1,39 @@
+package _05_self_tests._6_level0_summary;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Cat {
+	public static void main(String[] args) {
+//level 0
+	//robot:
+		Robot rob = new Robot();
+		rob.setSpeed(100);
+		rob.penDown();
+		for(int i = 0; i< 100; i++) {
+		rob.move(100);
+		rob.turn(54);
+		}
+	//Strings:
+	//JOptionPane
+	//print to console, use scanners
+//level 1
+		//main method
+		//color
+		//if statements
+		//int
+		//increment
+		//for loop
+//level 2
+		//random
+		//call methods
+		//integer.parseInt
+//level 3
+		//if / else if / else
+	//	for loop variables
+	//	modulo
+//level 4/5
+		//double, char, boolean
+		//nested loops
+		//writing loops
+	}
+}
